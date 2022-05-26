6486455a8eee9e068334763d920835e3778d504b
Kevin Anderson <kevinand@protonmail.com>
2022-05-26 10:47:37 -0500

    More scheme stuff

--- .emacs.d/package-setup.el
+++ .emacs.d/package-setup.el
@@ -1,8 +1,5 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
-;; and `package-pinned-packages`. Most users will not need or want to do this.
-;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 
 (when (not package-archive-contents)
@@ -20,14 +17,11 @@
     geiser
     geiser-chez
     geiser-chibi
-    geiser-chicken
     geiser-gambit
-    geiser-gauche
     geiser-guile
     geiser-kawa
     geiser-mit
     geiser-racket
-    geiser-stklos
     helm-lsp
     helm-xref
     hydra
